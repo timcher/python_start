147d42852ce132c78fe2beb2e5c24ed5e41ede62
timcher <timch90@mail.ru>
2018-03-17 21:07:57 +0300

    Change task_1

--- task_1.py
+++ task_1.py
@@ -1,4 +1,8 @@
-square=10*100
-garden=15*25
-free=str(square-garden)
-print(free+" "+"квадратных метров")
+area_s = 10 
+gr_h = 15 
+gr_w = 25 
+
+area_s *= 100 
+gr_s = gr_h * gr_w 
+
+print(area_s - gr_s)
