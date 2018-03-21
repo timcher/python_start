7fd10d2eadc3cc109261ffbf793f5ef6afc68db3
timcher <timch90@mail.ru>
2018-03-21 20:05:35 +0300

    change task_3

--- task_3.py
+++ task_3.py
@@ -3,22 +3,25 @@ a_x=int(input("Введите координату X вершины A треуг
 a_y=int(input("Введите координату Y вершины A треугольника ABC: "))
 b_x=int(input("Введите координату X вершины B треугольника ABC: "))
 b_y=int(input("Введите координату Y вершины B треугольника ABC: "))
-while b_x==a_x and b_y==a_y:
+while b_x==a_x and b_y==a_y: 
     if b_y==a_y and b_x==a_x:
         b_x=int(input("Введите координату X вершины B треугольника ABC: "))
         b_y=int(input("Введите координату X вершины B треугольника ABC: "))
 c_x=int(input("Введите координату X вершины C треугольника ABC: "))
 c_y=int(input("Введите координату Y вершины C треугольника ABC: "))
-while b_x==c_x or a_x==c_x and b_y==c_y or c_y==c_y:
+while b_x==c_x or a_x==c_x and b_y==c_y or a_y==c_y:
     if b_x==c_x and b_y==c_y:
         c_x=int(input("Введите координату X вершины C треугольника ABC: "))
         c_y=int(input("Введите координату Y вершины C треугольника ABC: "))
     elif a_x==c_x and a_y==c_y:
         c_x=int(input("Введите координату X вершины C треугольника ABC: "))
         c_y=int(input("Введите координату Y вершины C треугольника ABC: "))
-ab=(b_x-a_x)**2+(b_y-a_y)**2
-bc=(c_x-b_x)**2+(c_y-b_y)**2
-ac=(c_x-a_x)**2+(c_y-a_y)**2
+if b_x-a_x != 0 and b_y-a_y != 0:
+    ab=(b_x-a_x)**2+(b_y-a_y)**2
+if c_x-b_x != 0 and c_y-b_y != 0:
+    bc=(c_x-b_x)**2+(c_y-b_y)**2
+if c_x-a_x != 0 and c_y-a_y != 0:
+    ac=(c_x-a_x)**2+(c_y-a_y)**2
 l_ab=math.sqrt(ab)
 l_bc=math.sqrt(bc)
 l_ac=math.sqrt(ac)
